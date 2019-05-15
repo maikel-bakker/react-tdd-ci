4cf347ea448f74fec919918a64ada128f00cb0b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-15 12:51:10 +0000

    Deploy maikel-bakker/react-tdd-ci to github.com/maikel-bakker/react-tdd-ci.git:gh-pages

---
+++ precache-manifest.96733ff18375497b56841c762d14b407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cd29b881fd388a9b4b103b2e36590d",
+    "url": "/react-tdd-ci/index.html"
+  },
+  {
+    "revision": "08e50aaf747598092e33",
+    "url": "/react-tdd-ci/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "ad76c7b1eb6f0f8bed51",
+    "url": "/react-tdd-ci/static/js/2.735923fe.chunk.js"
+  },
+  {
+    "revision": "08e50aaf747598092e33",
+    "url": "/react-tdd-ci/static/js/main.e6b729a5.chunk.js"
+  },
+  {
+    "revision": "d2a35dc90530c877d917",
+    "url": "/react-tdd-ci/static/js/runtime~main.d43a3f17.js"
+  }
+]);
